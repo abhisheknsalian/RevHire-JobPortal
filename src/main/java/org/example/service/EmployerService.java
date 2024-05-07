66c2ba4f4d6ff95403712fc85db1da7e871debe4
abhisheknsalian <abhisheknsalian@gmail.com>
2024-05-07 07:37:19 +0530

    Project added

---
+++ src/main/java/org/example/service/EmployerService.java
@@ -0,0 +1,23 @@
+package org.example.service;
+
+import org.example.dao.User;
+import org.example.model.EmployerModel;
+
+public class EmployerService {
+
+    private User employer;
+
+    public EmployerService(User userDAO) {
+
+        this.employer = userDAO;
+    }
+
+    public void validateEmployerLogin(){
+
+        employer.validateEmployerLogin();
+    }
+    public  void EmployerRegister(EmployerModel employerModel){
+
+        employer.EmployerRegister(employerModel);
+    }
+}
